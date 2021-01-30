c56a14b6c11e1e66fcf71688f530a0fba669e89a
Fumiya Nakagawa <nakagawa@dptr.jp>
2021-01-30 22:51:05 +0900

    ステータスごとにタスクを分ける

--- src/reducers/todos.ts
+++ src/reducers/todos.ts
@@ -1,31 +1,58 @@
-import { TodoAction, TODO_ADD } from '../actions/index';
+import { TodoAction, TODO_ADD } from "../actions/index";
 
 export interface Todo {
-  id?: number,
-  text?: string,
+  id?: number;
+  text?: string;
 }
 
-export interface TodoState {
-  status: string;
-  tasks: Todo[];
-}
+// export interface TodoState {
+//   status: string;
+//   tasks: Todo[];
+// }
 
-const initialState: TodoState = {status: 'In Progress', tasks: []};
+// const initialState: TodoState = {status: 'In Progress', tasks: []};
 
 // TODO: statusごとにタスク分ける
+// const todoReducer = (
+//   state: TodoState = initialState,
+//   action: TodoAction
+// ): TodoState => {
+//   switch (action.type) {
+//     case TODO_ADD:
+//       return {
+//         ...state,
+//         tasks: [...state.tasks, action.payload],
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+export interface TodoState {
+  backlog: Todo[];
+  inprogress: Todo[];
+  done: Todo[];
+}
+
+const initialState: TodoState = {
+  backlog: [],
+  inprogress: [],
+  done: [],
+};
+
 const todoReducer = (
   state: TodoState = initialState,
-  action: TodoAction,
+  action: TodoAction
 ): TodoState => {
   switch (action.type) {
     case TODO_ADD:
       return {
         ...state,
-        tasks: [...state.tasks, action.payload]
-      }
-      default:
-        return state
+        backlog: [...state.backlog, action.payload],
+      };
+    default:
+      return state;
   }
-}
+};
 
-export default todoReducer
+export default todoReducer;
