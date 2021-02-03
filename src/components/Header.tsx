c323aab06616d93a50af7e7c43b9ac530ceb672f
Fumiya Nakagawa <nakagawa@dptr.jp>
2021-02-03 09:42:11 +0900

    create search field

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,20 @@
 import React, { FC } from 'react';
 import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
 
 const Header: FC<{}> = () => {
   return (
     <Grid item xs={12}>
       <header>
-        <p>header</p>
+        <h1>Task App</h1>
+        <form noValidate autoComplete="off">
+          <TextField id="standard-basic" label="Search" />
+          <IconButton aria-label="search">
+            <SearchIcon/>
+          </IconButton>
+        </form>
       </header>
     </Grid>
   );
