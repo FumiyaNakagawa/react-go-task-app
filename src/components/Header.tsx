8c355678de78cb324a7e6ae7db664ae9a4aaaf84
Fumiya Nakagawa <nakagawa@dptr.jp>
2021-02-03 22:52:26 +0900

    ヘッダーの修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,28 @@
-import React, { FC } from 'react';
-import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/TextField';
-import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
+import React, { FC } from "react";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
+import styled from "styled-components";
 
 const Header: FC<{}> = () => {
   return (
     <Grid item xs={12}>
-      <header>
+      <StyledHeader>
         <h1>Task App</h1>
         <form noValidate autoComplete="off">
           <TextField id="standard-basic" label="Search" />
           <IconButton aria-label="search">
-            <SearchIcon/>
+            <SearchIcon />
           </IconButton>
         </form>
-      </header>
+      </StyledHeader>
     </Grid>
   );
-}
+};
+
+const StyledHeader = styled.header`
+  display: flex;
+`;
 
 export default Header;
