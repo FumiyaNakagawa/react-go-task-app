16e3d376215333d9d96b7b534378ddc7c5b16901
Fumiya Nakagawa <nakagawa@dptr.jp>
2021-02-07 21:39:22 +0900

    fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,19 @@
-import React, { FC } from 'react';
-import Grid from '@material-ui/core/Grid';
-
+import React, { FC } from "react";
+import Grid from "@material-ui/core/Grid";
+import styled from "styled-components";
 
 const Footer: FC<{}> = () => {
   return (
     <Grid item xs={12}>
-      <header>
-        <p>Footer</p>
-      </header>
+      <StyledFooter>
+        <p>&copy; 2021 task app.</p>
+      </StyledFooter>
     </Grid>
   );
-}
+};
 
-export default Footer
+const StyledFooter = styled.footer`
+  text-align: center;
+`;
+
+export default Footer;
